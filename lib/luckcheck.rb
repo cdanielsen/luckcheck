1afed8619d4a831c8d9bc633c94752b1fbc46d25
Christian <ckdanielsen@gmail.com>
2014-08-02 23:22:53 +0000

    Write and pass test for three digit strings

--- lib/luckcheck.rb
+++ lib/luckcheck.rb
@@ -1,12 +1,26 @@
 def luckcheck(string_of_num)
+  isOdd = false
   numbers = []
   string_of_num.split('').each do |num|
     numbers << num.to_i
   end
-  if numbers[0] == numbers[1]
-    result = true
+  
+  if numbers.length % 2 != 0
+    isOdd = true
+  end
+  
+  if isOdd
+    if (numbers[0] == numbers[2])
+      result = true
+    else
+      result = false
+    end
   else
-    result = false
+    if (numbers[0] == numbers[1])
+      result = true
+    else
+      result =false
+    end
   end
   result
 end
--- spec/luckcheck_spec.rb
+++ spec/luckcheck_spec.rb
@@ -5,4 +5,7 @@ describe 'luckcheck' do
   it('given a two digit string, returns true if the two digits are equal') do
     expect(luckcheck('11')).to eq(true)
 	end
+  it('given a three digit string, returns true if the first and third digits are equal') do
+    expect(luckcheck('121')).to eq(true)
+  end
 end
